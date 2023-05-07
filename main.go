7dbcd1d233f81df318906509f1acc6b50960192b
Narumi-Sawasaki <sawasaki@throo.io>
2023-05-08 03:27:02 +0900

    一応動く

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
 
 	"github.com/hashicorp/go-envparse"
 	"github.com/urfave/cli/v2"
@@ -20,34 +21,17 @@ import (
 
 type ParsedRes map[string]interface{}
 
-func callGetApi() (ParsedRes, error) {
-	repoRoot, err := getRepoRoot()
-	if err != nil {
-		return nil, err
-	}
-
-	trellorcPath := repoRoot + "/.trellorc"
-	file, err := os.Open(trellorcPath)
-	if err != nil {
-		return nil, err
-	}
-	env, err := envparse.Parse(bufio.NewReader(file))
-	if err != nil {
-		return nil, err
-	}
-
-	url := fmt.Sprintf("https://api.trello.com/1/search?query=board:%s%%2029&key=%s&token=%s", env["BOARD_ID"], env["API_KEY"], env["API_TOKEN"])
-	resp, _ := http.Get(url)
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-
-	var data ParsedRes
-	if err := json.Unmarshal(body, &data); err != nil {
-		return nil, err
-	}
-
-	return data, nil
-}
+// func callGetApi() (ParsedRes, error) {
+// 	url := fmt.Sprintf("https://api.trello.com/1/search?query=board:%s%%2029&key=%s&token=%s", env["BOARD_ID"], env["API_KEY"], env["API_TOKEN"])
+// 	resp, _ := http.Get(url)
+// 	defer resp.Body.Close()
+// 	body, _ := io.ReadAll(resp.Body)
+// 	var data ParsedRes
+// 	if err := json.Unmarshal(body, &data); err != nil {
+// 		return nil, err
+// 	}
+// 	return data, nil
+// }
 
 type QueryParams map[string]string
 type Config map[string]string
@@ -60,8 +44,15 @@ type ApiClientImpl struct {
 
 func (client *ApiClientImpl) Get(url string, queryParams QueryParams) (ParsedRes, error) {
 	// queryParamsを組み立てる
-	url := fmt.Sprintf("%s?query=board:%s%%2029&key=%s&token=%s", env["BOARD_ID"], env["API_KEY"], env["API_TOKEN"])
-	resp, _ := http.Get(url)
+	keyValues := []string{}
+	for key, value := range queryParams {
+		keyValues = append(keyValues, key+"="+value)
+	}
+	urlWithParams := url
+	if len(keyValues) > 0 {
+		urlWithParams += fmt.Sprintf("?%s", strings.Join(keyValues, "&"))
+	}
+	resp, _ := http.Get(urlWithParams)
 	defer resp.Body.Close()
 	body, _ := io.ReadAll(resp.Body)
 
@@ -74,22 +65,40 @@ func (client *ApiClientImpl) Get(url string, queryParams QueryParams) (ParsedRes
 
 }
 
-func fetchCards(client ApiClient, keyword string) (ParsedRes, error) {
+func fetchCards(client ApiClient, config Config, keyword string) (ParsedRes, error) {
 	return client.Get("https://api.trello.com/1/search", QueryParams{
-		"key": keyword,
+		"query": fmt.Sprintf("board:%s%%20%s", config["BOARD_ID"], keyword),
+		"key":   config["API_KEY"],
+		"token": config["API_TOKEN"],
 	})
 }
 
 func showCard(_ *cli.Context) error {
-	res, err := callGetApi()
+	repoRoot, err := getRepoRoot()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		return err
+	}
+	trellorcPath := repoRoot + "/.trellorc"
+	file, err := os.Open(trellorcPath)
+	if err != nil {
+		return err
+	}
+	env, err := envparse.Parse(bufio.NewReader(file))
+	if err != nil {
+		return err
 	}
 
 	currentTaskName, err := getCurrentTaskName()
 	if err != nil {
 		return err
 	}
+
+	client := &ApiClientImpl{}
+	res, err := fetchCards(client, Config(env), currentTaskName)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
 	for _, v := range res["cards"].([]interface{}) {
 		card := v.(map[string]interface{})
 
